c2c7c2fed7a3cbe41de444315fe5405f1e70459f
DRONIC <DRONIC@DRONIC-NB.zg.local>
2023-10-31 16:00:30 +0100

    Skill impl da configurare

---
+++ src/main/java/com/candidatoDB/pw2/interfaces/impl/SkillIMPL.java
@@ -0,0 +1,40 @@
+package com.candidatoDB.pw2.interfaces.impl;
+
+import java.util.List;
+
+import com.candidatoDB.pw2.entity.Skill;
+import com.candidatoDB.pw2.interfaces.SkillDAO;
+
+public class SkillIMPL implements SkillDAO{
+
+	@Override
+	public void save(Skill skill) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Skill skill) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Skill skill) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Skill> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Skill findById(int id_skill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
