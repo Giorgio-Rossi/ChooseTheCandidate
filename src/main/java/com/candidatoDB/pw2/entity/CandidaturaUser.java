aa2ac3dbce30806dcf77b529e1cc46a621704ec9
GiorgioRossi00 <giorgio.rossi@zucchetti.it>
2023-10-31 10:05:34 +0100

    mancava una } nella classe candidaturaUser

--- src/main/java/com/candidatoDB/pw2/entity/CandidaturaUser.java
+++ src/main/java/com/candidatoDB/pw2/entity/CandidaturaUser.java
@@ -45,6 +45,7 @@ public class CandidaturaUser {
 	
 	public void setId_user(int id_user) {
 		this.id_user = id_user;
+	}
 	
 	public Date getData_candidatura() {
 		return data_candidatura;
@@ -63,3 +64,5 @@ public class CandidaturaUser {
 	
 	
 }
+	
+
