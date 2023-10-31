faddcf47f81ef5927ed12a343cce31fc558f53a0
GiorgioRossi00 <giorgio.rossi@zucchetti.it>
2023-10-31 11:39:39 +0100

    Aggiornato il file insert aggiungendo alcuni utenti e citta

---
+++ src/main/resources/Insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO Utente VALUES ('admin', 'admin', null,null, null, null, null, null, null, 'admin', 'admin' );
+INSERT INTO Utente VALUES ('Giorgio', 'Rossi', null, null, null, null, null, null, null, 'admin', 'admin' );
+INSERT INTO Utente VALUES ('Nicolo', 'Drovandi', null,null, null, null, null, null, null, 'admin', 'admin' );
+
+INSERT INTO Utente VALUES ('matteo','salvo','CF1','matteosalvo@gmail.com','20000203','Via Marconi 2','1','17900','347689876','user','salvini');
+INSERT INTO Utente VALUES ('fiona','giusti','CF2','fionagiusti@gmail.com','20030806','Via Roma 29','1','16177','349680856','user','fiona');
+INSERT INTO Utente VALUES ('gianluca','torre','CF3','gianlu@gmail.com','19800407','Via Milano 56','2','20321','387689806','user','torre');
+INSERT INTO Utente VALUES ('marta','lupu','CF4','mlupu@gmail.com','20030203','Via Rodi 70','2','16167','339880856','user','marta');
+
+
+INSERT INTO Citta VALUES ('liguria','genova');
+INSERT INTO Citta VALUES ('lombardia','milano');
+INSERT INTO Citta VALUES ('lazio','roma');
+INSERT INTO Citta VALUES ('campania','napoli');
+INSERT INTO Citta VALUES ('lombardia','lodi');
