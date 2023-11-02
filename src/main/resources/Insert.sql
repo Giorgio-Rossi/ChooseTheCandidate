97c35888330180d02772f5e86f02a8b22775db28
DRONIC <DRONIC@DRONIC-NB.zg.local>
2023-11-02 10:19:13 +0100

    Update insert Db

--- src/main/resources/Insert.sql
+++ src/main/resources/Insert.sql
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-INSERT INTO Utente VALUES ('admin', 'admin', null,null, null, null, null, null, null, 'admin', 'admin' );
-INSERT INTO Utente VALUES ('Giorgio', 'Rossi', null, null, null, null, null, null, null, 'admin', 'admin' );
-INSERT INTO Utente VALUES ('Nicolo', 'Drovandi', null,null, null, null, null, null, null, 'admin', 'admin' );
-INSERT INTO Utente VALUES ('Marco','Rossi','CF156','marcorossi@gmail.com','20000203','Via Marconi 8','1','17900','347689876','user','salvini');
-
-
 INSERT INTO Utente VALUES ('admin', 'admin', null,'admin@gmail.com', null, null, null, null, null, 'admin', 'admin' );
 INSERT INTO Utente VALUES ('Giorgio', 'Rossi', null, 'giorgiorossi@gmail.com', null, null, null, null, null, 'admin', 'admin' );
 INSERT INTO Utente VALUES ('Nicolo', 'Drovandi', null,'nicolodrovandi@gmail.com', null, null, null, null, null, 'admin', 'admin' );
@@ -13,15 +6,19 @@ INSERT INTO Utente VALUES ('Luca', 'Sanguinetti', null, 'lucasanguinetti@gmail.c
 INSERT INTO Utente VALUES ('Claudio', 'Balzani', null,'claudiobalzani@gmail.com', null, null, null, null, null, 'admin', 'admin' );
 INSERT INTO Utente VALUES ('Biagio', 'Ricci', null,'biagioricci@gmail.com', null, null, null, null, null, 'admin', 'admin' );
 
+insert into Utente values ('Matteo','Salvo','CF1','matteosalvo@gmail.com','20000203','Via Marconi 2','1','17900','347689876','user','salvini');
+insert into Utente values ('Fiona','Giusti','CF2','fionagiusti@gmail.com','20030806','Via Roma 29','1','16177','349680856','user','fiona');
+insert into Utente values ('Gianluca','Torre','CF3','gianlu@gmail.com','19800407','Via Milano 56','2','20321','387689806','user','torre');
+insert into Utente values ('Marta','Lupu','CF4','mlupu@gmail.com','20030203','Via Rodi 70','2','16167','339880856','user','marta');
 
-INSERT INTO Utente VALUES ('matteo','salvo','CF1','matteosalvo@gmail.com','20000203','Via Marconi 2','1','17900','347689876','user','salvini');
-INSERT INTO Utente VALUES ('fiona','giusti','CF2','fionagiusti@gmail.com','20030806','Via Roma 29','1','16177','349680856','user','fiona');
-INSERT INTO Utente VALUES ('gianluca','torre','CF3','gianlu@gmail.com','19800407','Via Milano 56','2','20321','387689806','user','torre');
-INSERT INTO Utente VALUES ('marta','lupu','CF4','mlupu@gmail.com','20030203','Via Rodi 70','2','16167','339880856','user','marta');
 
 
-INSERT INTO Citta VALUES ('liguria','genova');
-INSERT INTO Citta VALUES ('lombardia','milano');
-INSERT INTO Citta VALUES ('lazio','roma');
-INSERT INTO Citta VALUES ('campania','napoli');
-INSERT INTO Citta VALUES ('lombardia','lodi');
+insert into Citta values ('Liguria','Genova');
+insert into Citta values ('Lombardia','Milano');
+insert into Citta values ('Lazio','Roma');
+insert into Citta values ('Campania','Napoli');
+insert into Citta values ('Lombardia','Lodi');
+insert into Citta values ('Piemonte','Torino');
+insert into Citta values ('Veneto','Venezia');
+insert into Citta values ('Toscana','Firenze');
+insert into Citta values ('Emilia Romagna','Bologna');
