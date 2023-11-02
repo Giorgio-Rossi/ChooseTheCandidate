fa82516cf52df4a80fb6f9fa2228d58be46915a2
DRONIC <DRONIC@DRONIC-NB.zg.local>
2023-11-02 14:53:10 +0100

    Nuova classe impl posizione
    Merge branch 'master' of https://github.com/Lollof00/PW2.git

---
+++ src/main/java/com/candidatoDB/pw2/interfaces/impl/PosizioneIMPL.java
@@ -0,0 +1,17 @@
+package com.candidatoDB.pw2.interfaces.impl;
+
+import java.util.List;
+
+import com.candidatoDB.pw2.entity.Citta;
+import com.candidatoDB.pw2.entity.Posizione;
+import com.candidatoDB.pw2.interfaces.PosizioneDAO;
+
+public class PosizioneIMPL implements PosizioneDAO{
+
+	@Override
+	public List<Posizione> searchByCity(Citta citta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
