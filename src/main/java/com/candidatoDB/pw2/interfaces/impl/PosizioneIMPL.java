6864cc412a60485bf5b19e57f8e92281e86b8f22
DRONIC <DRONIC@DRONIC-NB.zg.local>
2023-11-02 15:20:36 +0100

    Aggiunto metodo in PosizioneIMPL

--- src/main/java/com/candidatoDB/pw2/interfaces/impl/PosizioneIMPL.java
+++ src/main/java/com/candidatoDB/pw2/interfaces/impl/PosizioneIMPL.java
@@ -1,17 +1,76 @@
 package com.candidatoDB.pw2.interfaces.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import com.candidatoDB.pw2.entity.CategoriaPosizione;
 import com.candidatoDB.pw2.entity.Citta;
 import com.candidatoDB.pw2.entity.Posizione;
+import com.candidatoDB.pw2.entity.Quiz;
 import com.candidatoDB.pw2.interfaces.PosizioneDAO;
+import com.servlets.pw2.controller.DBUtil;
+import com.servlets.pw2.controller.SQLServerConnection;
 
-public class PosizioneIMPL implements PosizioneDAO{
+public class PosizioneIMPL implements PosizioneDAO {
+	private SQLServerConnection connection = new SQLServerConnection();
+
+	public PosizioneIMPL() {
+		connection.Connect();
+	}
 
 	@Override
 	public List<Posizione> searchByCity(Citta citta) {
-		// TODO Auto-generated method stub
-		return null;
+        List<Posizione> posizioni = new ArrayList<>();
+      
+        String sql = "SELECT * FROM Posizione WHERE id_citta = ?";
+        PreparedStatement statement = null;
+		ResultSet resultSet = null;
+        try {
+    
+  
+           
+    		
+    		statement.setInt(1, citta.getId_citta());
+
+             resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Posizione posizione = new Posizione();
+        		posizione.setId_poszione(resultSet.getInt(1));
+				posizione.setN_ammissioni(resultSet.getInt(2));
+				posizione.setDescrizione(resultSet.getString(3));
+				 citta = new Citta();
+				citta.setId_citta(resultSet.getInt(4));
+				posizione.setCitta(citta);
+				CategoriaPosizione categoriaPosizione = new CategoriaPosizione();
+				categoriaPosizione.setId_categoria(resultSet.getInt(5));
+				posizione.setCategoria(categoriaPosizione);
+				Quiz quiz = new Quiz();
+				quiz.setId_quiz(resultSet.getInt(6));
+				posizione.setQuiz(quiz);
+				posizione.setStato(resultSet.getString(7));
+     posizione.setData_inserimento(new java.sql.Date(resultSet.getDate(8).getTime()));
+     posizione.setRuolo(resultSet.getString(9));
+
+            
+                posizioni.add(posizione);
+            }
+            } catch (SQLException e) {
+    			System.err.println(e.getMessage());
+    		} finally {
+    			DBUtil.close(resultSet);
+    			DBUtil.close(statement);
+    			DBUtil.close((Connection) connection);
+    		}
+
+        return posizioni;
+    
+}
 	}
 
-}
+
+
