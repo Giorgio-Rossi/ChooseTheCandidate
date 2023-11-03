307490eb5ca61780ca5182536453ceee9d06026c
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 <claudio.balzani@zucchetti.it>
2023-11-03 09:54:21 +0100

    d

--- src/main/java/com/logout/Logout.java
+++ src/main/java/com/logout/Logout.java
@@ -16,6 +16,9 @@ public class Logout extends HttpServlet {
         if(req.getSession().getAttribute("ruolo").equals("user")){
             req.getSession().invalidate();
             resp.sendRedirect("login.jsp");
+            resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+            resp.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+            resp.setDateHeader("Expires", 0);
         }else {
             req.getSession().invalidate();
             resp.sendRedirect("login.jsp");
